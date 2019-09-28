54de4e111ae2856bef3d0a6017ed51412f484463
Charles-Henri Sauget <contact@sauget-ch.fr>
2019-09-28 21:01:24 +0200

    Change config

--- AzureDataToolsStandard/Helpers/ADALHelper.cs
+++ AzureDataToolsStandard/Helpers/ADALHelper.cs
@@ -26,25 +26,5 @@ namespace Functions.Helpers
                 return authenticationResult.AccessToken;
             }
         }
-        public static string GetPBIToken(string PowerBILogin, string PowerBIPassword, string AuthenticationContextUrl, string PowerBIRessourceUrl, string ClientId)
-        {
-            UserCredential credential = new UserPasswordCredential(PowerBILogin, PowerBIPassword);
-
-            // Authenticate using created credentials
-            AuthenticationContext authenticationContext = new AuthenticationContext("https://login.microsoftonline.com/b2ed466b-1ad5-4ea5-b19e-10f2abc41e7c");
-
-            Task<AuthenticationResult> authenticationResultTask = authenticationContext.AcquireTokenAsync(PowerBIRessourceUrl, ClientId, credential);
-
-            AuthenticationResult authenticationResult = authenticationResultTask.Result;
-            if (authenticationResult == null)
-            {
-                throw new Exception("Authentication Failed.");
-            }
-            else
-            {
-                return authenticationResult.AccessToken;
-            }
-
-        }
     }
 }
